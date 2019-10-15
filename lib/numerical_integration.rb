f625974a5df407e8e0c39622ff76c029426277ba
D Drozdov <ds-drozdov@yandex.ru>
2019-10-15 09:16:57 +0300

    Adds files for numerical integration

---
+++ lib/numerical_integration.rb
@@ -0,0 +1,17 @@
+module Silicium
+  class NumericalIntegration
+    # Delevoper: Dima
+    def three_eights_integration(a, b, block)
+      n = 1000
+      dx = (b - a) / n
+      result = 0
+
+      i = 0
+      n.times do
+
+      end
+    end
+
+    # Write your methods here
+  end
+end
---
+++ test/numerical_integration_test.rb
@@ -0,0 +1,5 @@
+require "test_helper"
+
+class NumericalIntegrationTest < Minitest::Test
+  # Write your tests here
+end
