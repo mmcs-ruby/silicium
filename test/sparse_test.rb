1757ca69573047698bccc30fad183889685e628b
dr3amy <arterterone@gmail.com>
2019-10-15 09:43:03 +0300

    Added spaerse_test.rb

--- test/sparse_test.rb
+++ test/sparse_test.rb
@@ -1,1 +1,15 @@
+require "test_helper"
+
+class SparseTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Silicium::VERSION
+  end
+
+  def test_that_add_works
+    m = ::Silicium::SparseMatrix.new(3, 3)
+    m.add(0, 0, 1)
+
+    assert_equal [[0, 0, 1]], m.triplets
+  end
+end
 
