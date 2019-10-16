88a67aa61cf6ea9c7a12c2f41316a2685668dbe1
D Drozdov <ds-drozdov@yandex.ru>
2019-10-16 13:19:37 +0300

    Three eights integration done. Tests written

--- lib/numerical_integration.rb
+++ lib/numerical_integration.rb
@@ -1,15 +1,18 @@
 module Silicium
   class NumericalIntegration
     # Delevoper: Dima
-    def three_eights_integration(a, b, block)
-      n = 1000
-      dx = (b - a) / n
-      result = 0
-
-      i = 0
+    def self.three_eights_integration(a, b, &block)
+      n = 10000
+      dx = 1.0 * (b - a) / n
+      result = 0.0
+      x = a
       n.times do
-
+        result +=
+            (block.call(x) + 3 * block.call((2 * x + x + dx) / 3.0) +
+                3 * block.call((x + 2 * (x + dx)) / 3.0) + block.call(x + dx)) / 8.0 * dx
+        x += dx
       end
+      result
     end
 
     # Write your methods here
