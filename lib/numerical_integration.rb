550d369b6a8e21e1254b9beceac7c31bbde57c67
ivolnoboi <irinavolnoboy@gmail.com>
2019-10-17 20:58:09 +0300

    Simpson integration was written

--- lib/numerical_integration.rb
+++ lib/numerical_integration.rb
@@ -15,9 +15,21 @@ module Silicium
       result
     end
 
-    
-    # Write your methods here
-    def simpson_integration(a, b, eps = 0.0001, &block)
+
+    # Simpson Integration
+    def self.simpson_integration(a, b, eps = 0.0001, &block)
+      if a > b
+        a, b = b, a
+      end
+      n = (1 / eps).floor
+      dx = (b - a) * eps
+      result = 0
+      i = 0
+      while i < n do
+        result += (block.call(a + i * dx) + 4 * block.call(((a + i * dx) +
+            (a + (i + 1) * dx)) / 2.0) + block.call(a + (i + 1) * dx)) / 6.0 * dx
+        i += 1
+      end
       result
     end
   end
