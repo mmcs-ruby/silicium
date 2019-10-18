6c765d419fe446a1a8fc87c18ce5269d25722c90
Damir Kazeev <dr.kazeev@yandex.ru>
2019-10-18 15:19:01 +0300

    activsion func done

--- lib/optimization.rb
+++ lib/optimization.rb
@@ -7,6 +7,11 @@ module Silicium
     def re_lu(x)
       x.negative? ? 0 : x
     end
-  end
 
+    #sigmoid function
+    def sigmoid(x)
+      1/(1+Math.exp(-x))
+    end
+
+  end
 end
--- test/optimization_test.rb
+++ test/optimization_test.rb
@@ -7,5 +7,15 @@ class SiliciumTest < Minitest::Test
 
   def test_reLu
     assert_equal 0, re_lu(-1)
+    assert_equal 0.5, re_lu(0.5)
+    assert_equal 2, re_lu(2)
   end
+
+  def test_sigmoid
+    assert_equal 0.5, sigmoid(0)
+    (0..1).step(0.1) do |n|
+      assert (sigmoid(n) < 1)
+    end
+  end
+
 end
