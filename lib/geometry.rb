b7710ac1a8a5274d3005104f464e86e6e9b7f5b7
RobolabGs2 <slavikelk@yandex.ru>
2019-10-19 22:52:13 +0300

    Fix geometry.minimal_convex_hull_2d

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -18,6 +18,7 @@ module Silicium
       oriented_area(a, b, c).positive?
     end
 
+    # https://e-maxx.ru/algo/convex_hull_graham
     def minimal_convex_hull_2d(points)
       return points if points.empty? || points.size == 1 || points.size == 2
 
@@ -27,31 +28,31 @@ module Silicium
       up = [p1]
       down = [p1]
 
-      (1..points.size).each do |i|
-        if i == points.size - 1 || clockwise(p1, points[i], p2) do
-          while up.size >= 2 && !clockwise(up[up.size - 2], up[up.size - 1], points[i])
+      (1...points.size).each do |i|
+        point = points[i]
+        if i == points.size - 1 || clockwise(p1, point, p2)
+          while up.size >= 2 && !clockwise(up[up.size - 2], up[up.size - 1], point)
             up.pop
           end
-          up.push(points[i])
+          up.push(point)
         end
 
-        if i == points.size - 1 || counter_clockwise(p1, points[i], p2)
-          while down.size >= 2 && !counter_clockwise(down[down.size - 2], down[down.size - 1], points[i])
+        if i == points.size - 1 || counter_clockwise(p1, point, p2)
+          while down.size >= 2 && !counter_clockwise(down[down.size - 2], down[down.size - 1], point)
             down.pop
           end
-          down.push(points[i])
+          down.push(point)
         end
       end
-
-        hull = []
-        (0..up.size).each do |i|
-          hull.push(points[i])
-        end
-        ((down.size - 2)..0).each do |i|
-          hull.push(points[i])
-        end
-
+      hull = []
+      (0...up.size).each do |j|
+        hull.push(up[j])
       end
+      (1..(down.size - 2)).reverse_each do |j|
+        puts j
+        hull.push(down[j])
+      end
+      hull
     end
   end
 end
--- test/geometry_test.rb
+++ test/geometry_test.rb
@@ -15,13 +15,13 @@ class GeometryTest < Minitest::Test
   end
 
   def test_general
-    assert_equal([Point.new(-2, 1), Point.new(-1, 1),
-                  Point.new(1, -2), Point.new(2, 2)],
-                  minimal_convex_hull_2d([Point.new(-2, -1),
-                                                  Point.new(0, -1), Point.new(-1, 0),
-                                                  Point.new(-1, 1), Point.new(2, 2),
-                                                  Point.new(1, -2), Point.new(2, 0),
-                                                  Point.new(1, 1)]))
+    assert_equal_as_sets([Point.new(-2, -1), Point.new(-1, 1),
+                          Point.new(1, -2), Point.new(2, 0), Point.new(2, 2)],
+                         minimal_convex_hull_2d([Point.new(-2, -1),
+                                                 Point.new(0, -1), Point.new(-1, 0),
+                                                 Point.new(-1, 1), Point.new(2, 2),
+                                                 Point.new(1, -2), Point.new(2, 0),
+                                                 Point.new(1, 1)]))
   end
 end
 
