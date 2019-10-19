8dd4270f71daa0cc6a2c9b01ccc59a6690914ff8
ivolnoboi <irinavolnoboy@gmail.com>
2019-10-19 13:58:37 +0300

    add tests

--- test/numerical_integration_test.rb
+++ test/numerical_integration_test.rb
@@ -54,4 +54,39 @@ class NumericalIntegrationTest < Minitest::Test
     assert_in_delta Math.log(3.5),
                     ::Silicium::NumericalIntegration.simpson_integration(2, 7) { |x| 1 / x }, @@delta
   end
+
+  def test_sin_simpson_integration
+    assert_in_delta Math.sin(8) + Math.sin(10),
+                    ::Silicium::NumericalIntegration.simpson_integration(-10, 8) { |x| Math.cos(x) }, @@delta
+  end
+
+  def test_arctan_simpson_integration
+    assert_in_delta Math.atan(Math::PI),
+                    ::Silicium::NumericalIntegration.simpson_integration(0, Math::PI) { |x| 1 / (1 + x ** 2) }, @@delta
+  end
+
+  def test_arcsin_simpson_integration
+    assert_in_delta Math::PI / 6,
+                    ::Silicium::NumericalIntegration.simpson_integration(-0.5, 0) { |x| 1 / Math.sqrt(1 - x ** 2) }, @@delta
+  end
+
+  def test_something_scary_simpson_integration
+    assert_in_delta 442.818,
+                    ::Silicium::NumericalIntegration.simpson_integration(2, 5, 0.001) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.001
+  end
+
+  def test_reverse_simpson_integration
+    assert_in_delta -(Math.sin(3) + Math.sin(4)),
+                    ::Silicium::NumericalIntegration.simpson_integration(4, -3) { |x| Math.cos(x) }, @@delta
+  end
+
+  def test_one_point_simpson_integration
+    assert_in_delta 0,
+                    ::Silicium::NumericalIntegration.simpson_integration(42, 42) { |x| Math.sin(x) / x }, @@delta
+  end
+
+  def test_polynom_simpson_integration
+    assert_in_delta 16519216 / 3.0,
+                    ::Silicium::NumericalIntegration.simpson_integration(-10, 18) { |x| x ** 5 + 3 * x ** 2 + 18 * x - 160 }, @@delta
+  end
 end
