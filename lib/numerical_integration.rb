c5c9ff828794ba40eab376e24a1ded90f826d569
robertovna <g_arshakyan@list.ru>
2019-10-21 20:11:54 +0300

    Added middle rectangles with specified accuracy

--- lib/numerical_integration.rb
+++ lib/numerical_integration.rb
@@ -14,7 +14,7 @@ module Silicium
 
 
 		# Method Middle Rectangles with a segment
-		def	self.middle_rectangles(a, b, n = 10_000, &block)
+		def	self.middle_rectangles_with_a_segment(a, b, n: 10_000, &block)
 			dx = (b - a) / n.to_f
 			result = 0
 			i = 1
@@ -23,6 +23,18 @@ module Silicium
 				i += 1
 			end
 			result
+		end
+
+		# Method Middle Rectangles with specified accuracy
+		def	self.middle_rectangles(a, b, eps: 0.0001, &block)
+			n = 1
+			loop  do
+				result = middle_rectangles_with_a_segment(a, b, n, &block)
+				n *= 5
+				result1 = middle_rectangles_with_a_segment(a, b, n, &block)
+				break if (result - result1).abs < eps
+			end
+			result1
 		end
   end
 end
