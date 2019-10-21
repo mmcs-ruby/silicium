27413269a83b5661208161356274011f722be088
robertovna <g_arshakyan@list.ru>
2019-10-21 19:02:17 +0300

    Fixed middle rectangles

--- lib/numerical_integration.rb
+++ lib/numerical_integration.rb
@@ -12,18 +12,17 @@ module Silicium
       end
     end
 
-	# Developer: Gayane
-	def	self.middle_rectangles(a, b, &block)
-		n = 1000
-		dx = (b - a) / n
-		result = 0
-		
-		i = 1
-		n.times do
-			result += block.call(a + dx * i / 2) * dx
-			i += 1
+
+		# Method Middle Rectangles with a segment
+		def	self.middle_rectangles(a, b, n = 10_000, &block)
+			dx = (b - a) / n.to_f
+			result = 0
+			i = 1
+			n.times do
+				result += block.call(a + dx * i / 2) * dx
+				i += 1
+			end
+			result
 		end
-		result
-	end
   end
 end
