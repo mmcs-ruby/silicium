a9bc5ee5b490929054c3a13a37b306a3a2f2b020
robertovna <g_arshakyan@list.ru>
2019-10-21 23:14:58 +0300

    Fixed miidle_rectangles and added test, but need to fix

--- lib/numerical_integration.rb
+++ lib/numerical_integration.rb
@@ -1,33 +1,23 @@
 module Silicium
   class NumericalIntegration
-    # Delevoper: Dima
-    def three_eights_integration(a, b, block)
-      n = 1000
-      dx = (b - a) / n
-      result = 0
-
-      i = 0
-      n.times do
-
-      end
-    end
 
 
 		# Method Middle Rectangles with a segment
-		def	self.middle_rectangles_with_a_segment(a, b, n: 10_000, &block)
+		def	self.middle_rectangles_with_a_segment(a, b, n = 10_000, &block)
 			dx = (b - a) / n.to_f
 			result = 0
 			i = 1
 			n.times do
-				result += block.call(a + dx * i / 2) * dx
+				result += block.call(a + dx * (i + 1 / 2)) * dx
 				i += 1
 			end
 			result
 		end
 
 		# Method Middle Rectangles with specified accuracy
-		def	self.middle_rectangles(a, b, eps: 0.0001, &block)
+		def	self.middle_rectangles(a, b, eps = 0.0001, &block)
 			n = 1
+			result1 = 0
 			loop  do
 				result = middle_rectangles_with_a_segment(a, b, n, &block)
 				n *= 5
