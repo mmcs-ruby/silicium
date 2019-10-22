c709c326ee084cbd231f0ee9edead2f1b82f132b
robertovna <g_arshakyan@list.ru>
2019-10-22 22:28:59 +0300

    Add new method- trapezoid method

--- lib/numerical_integration.rb
+++ lib/numerical_integration.rb
@@ -91,30 +91,55 @@ module Silicium
       result * dx
     end
 
-	
 
-		# Method Middle Rectangles with a segment
-		def	self.middle_rectangles_with_a_segment(a, b, n = 10_000, &block)
-			dx = (b - a) / n.to_f
-			result = 0
-			i = 0
-			n.times do
-				result += block.call(a + dx * (i + 1 / 2)) * dx
-				i += 1
-			end
-			result
-		end
 
-		# Method Middle Rectangles with specified accuracy
-		def	self.middle_rectangles(a, b, eps = 0.0001, &block)
-			n = 1
-			begin
-				result = middle_rectangles_with_a_segment(a, b, n, &block)
-				n *= 5
-				result1 = middle_rectangles_with_a_segment(a, b, n, &block)
-			end	until (result - result1).abs < eps
-			(result + result1) / 2.0
-		end
+    # Middle Rectangles Method with a segment
+    def	self.middle_rectangles_with_a_segment(a, b, n, &block)
+      dx = (b - a) / n.to_f
+      result = 0
+      i = 0
+      n.times do
+        result += block.call(a + dx * (i + 1 / 2)) * dx
+        i += 1
+      end
+      result
+    end
+
+    # Middle Rectangles Method  with specified accuracy
+    def	self.middle_rectangles(a, b, eps = 0.0001, &block)
+      n = 1
+      begin
+        result = middle_rectangles_with_a_segment(a, b, n, &block)
+        n *= 5
+        result1 = middle_rectangles_with_a_segment(a, b, n, &block)
+      end	until (result - result1).abs < eps
+      (result + result1) / 2.0
+    end
+
+
+    # Trapezoid Method with a segment
+    def	self.trapezoid_with_a_segment(a, b, n, &block)
+      dx = (b - a) / n.to_f
+      result = 0
+      i = 1
+      (n - 1).times do
+        result += block.call(a + dx * i)
+        i += 1
+      end
+      result += (block.call(a) + block.call(b)) / 2.0
+      result * dx
+    end
+
+    # Trapezoid Method with specified accuracy
+    def	self.trapezoid(a, b, eps = 0.0001, &block)
+      n = 1
+      begin
+        result = trapezoid_with_a_segment(a, b, n, &block)
+        n *= 5
+        result1 = trapezoid_with_a_segment(a, b, n, &block)
+      end	until (result - result1).abs < eps
+      (result + result1) / 2.0
+    end
   end
 end
 
