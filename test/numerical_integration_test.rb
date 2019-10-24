ee3e3cb79db60e2154162f57b4000f872cf00af3
D Drozdov <ds-drozdov@yandex.ru>
2019-10-24 20:41:47 +0300

    Remove ::Silicium::

--- test/numerical_integration_test.rb
+++ test/numerical_integration_test.rb
@@ -7,95 +7,95 @@ class NumericalIntegrationTest < Minitest::Test
 
   def test_log_three_eights_integration
     assert_in_delta Math.log(3.5),
-                    ::Silicium::NumericalIntegration.three_eights_integration(2, 7) { |x| 1 / x }, @@delta
+                    NumericalIntegration.three_eights_integration(2, 7) { |x| 1 / x }, @@delta
   end
 
   def test_error_three_eights_integration
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(0, 7) { |x| 1 / x }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(0, 7) { |x| 1 / x }
     end
   end
 
   def test_nan_error_three_eights_integration
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(0, 1) { |x| 1 / Math.log(x) }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(0, 1) { |x| 1 / Math.log(x) }
     end
   end
 
   def test_domain_error_three_eights_integration
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(-8, 7) { |x| Math.sqrt(x) }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(-8, 7) { |x| Math.sqrt(x) }
     end
 
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(-8, 7) { |x| Math.log(x) }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(-8, 7) { |x| Math.log(x) }
     end
 
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(-6, 16) { |x| Math.asin(x + 6) }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(-6, 16) { |x| Math.asin(x + 6) }
     end
 
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(-1, 7) { |x| 1 / Math.sqrt(x) + 23 }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(-1, 7) { |x| 1 / Math.sqrt(x) + 23 }
     end
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(0, 2) { |x| Math.log(x) - Math.log(x) }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(0, 2) { |x| Math.log(x) - Math.log(x) }
     end
 
-    assert_raises ::Silicium::IntegralDoesntExistError do
-      ::Silicium::NumericalIntegration.three_eights_integration(0, 3) { |x| Math.log(x) / Math.log(x) }
+    assert_raises IntegralDoesntExistError do
+      NumericalIntegration.three_eights_integration(0, 3) { |x| Math.log(x) / Math.log(x) }
     end
   end
 
   def test_sin_three_eights_integration
     assert_in_delta Math.sin(8) + Math.sin(10),
-                    ::Silicium::NumericalIntegration.three_eights_integration(-10, 8) { |x| Math.cos(x) }, @@delta
+                    NumericalIntegration.three_eights_integration(-10, 8) { |x| Math.cos(x) }, @@delta
   end
 
   def test_arctan_three_eights_integration
     assert_in_delta Math.atan(Math::PI),
-                    ::Silicium::NumericalIntegration.three_eights_integration(0, Math::PI) { |x| 1 / (1 + x ** 2) }, @@delta
+                    NumericalIntegration.three_eights_integration(0, Math::PI) { |x| 1 / (1 + x ** 2) }, @@delta
   end
 
   def test_arcsin_three_eights_integration
     assert_in_delta Math::PI / 6,
-                    ::Silicium::NumericalIntegration.three_eights_integration(-0.5, 0) { |x| 1 / Math.sqrt(1 - x ** 2) }, @@delta
+                    NumericalIntegration.three_eights_integration(-0.5, 0) { |x| 1 / Math.sqrt(1 - x ** 2) }, @@delta
   end
 
 
   def test_something_scary_three_eights_integration
     assert_in_delta 442.818,
-                    ::Silicium::NumericalIntegration.three_eights_integration(2, 5, 0.001) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.001
+                    NumericalIntegration.three_eights_integration(2, 5, 0.001) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.001
   end
 
   def test_something_scary_accuracy_001_three_eights_integration
     assert_in_delta 442.82,
-                    ::Silicium::NumericalIntegration.three_eights_integration(2, 5, 0.01) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.01
+                    NumericalIntegration.three_eights_integration(2, 5, 0.01) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.01
   end
 
   def test_something_scary_accuracy_01_three_eights_integration
     assert_in_delta 442.8,
-                    ::Silicium::NumericalIntegration.three_eights_integration(2, 5, 0.1) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.1
+                    NumericalIntegration.three_eights_integration(2, 5, 0.1) { |x| (x ** 4 + Math.cos(x) + Math.sin(x)) / Math.log(x) }, 0.1
   end
 
   def test_reverse_three_eights_integration
     assert_in_delta (-(Math.sin(3) + Math.sin(4))),
-                    ::Silicium::NumericalIntegration.three_eights_integration(4, -3) { |x| Math.cos(x) }, @@delta
+                    NumericalIntegration.three_eights_integration(4, -3) { |x| Math.cos(x) }, @@delta
   end
 
   def test_one_point_three_eights_integration
     assert_in_delta 0,
-                    ::Silicium::NumericalIntegration.three_eights_integration(42, 42) { |x| Math.sin(x) / x }, @@delta
+                    NumericalIntegration.three_eights_integration(42, 42) { |x| Math.sin(x) / x }, @@delta
   end
 
   def test_polynom_three_eights_integration
     assert_in_delta 16519216 / 3.0,
-                    ::Silicium::NumericalIntegration.three_eights_integration(-10, 18) { |x| x ** 5 + 3 * x ** 2 + 18 * x - 160 }, @@delta
+                    NumericalIntegration.three_eights_integration(-10, 18) { |x| x ** 5 + 3 * x ** 2 + 18 * x - 160 }, @@delta
   end
 
   def test_polynom_accuracy_three_eights_integration
     assert_in_delta 16519216 / 3.0,
-                    ::Silicium::NumericalIntegration.three_eights_integration(-10, 18, 0.00001) { |x| x ** 5 + 3 * x ** 2 + 18 * x - 160 }, 0.00001
+                    NumericalIntegration.three_eights_integration(-10, 18, 0.00001) { |x| x ** 5 + 3 * x ** 2 + 18 * x - 160 }, 0.00001
   end
 
 # TODO: Write tests with non-determined function (such as integral of 1/x from -1 to 1)
