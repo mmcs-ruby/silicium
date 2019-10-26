7772e5c3a06aad116a6aec17141aba6b1588171d
Mikhail <mikes22@yandex.ru>
2019-10-26 23:44:59 +0300

    Support for negative values in bar chart plotter

--- lib/plotter.rb
+++ lib/plotter.rb
@@ -5,13 +5,13 @@ module Silicium
   module Plotter
 
     class Image
-      def initialize(width, height)
-        @image = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::TRANSPARENT)
+      def initialize(width, height, bg_color = ChunkyPNG::Color::TRANSPARENT)
+        @image = ChunkyPNG::Image.new(width, height, bg_color)
       end
 
       def rectangle(x, y, width, height, color)
-        x_end = x + width
-        y_end = y + height
+        x_end = x + width - 1
+        y_end = y + height - 1
         (x..x_end).each do |i|
           (y..y_end).each do |j|
             @image[i, j] = color
@@ -19,18 +19,24 @@ module Silicium
         end
       end
 
-      def bar_chart(bars, bar_width, color)
+      def bar_chart(bars, bar_width, bars_color = ChunkyPNG::Color('red @ 1.0'), axis_color = ChunkyPNG::Color::BLACK)
         padding = 5
-        minx = bars.collect { |k, _| k }.min
-        maxx = bars.collect { |k, _| k }.max
-        miny = bars.collect { |_, v| v }.min
-        maxy = bars.collect { |_, v| v }.max
-        rectangle(padding, @image.height - padding, @image.width - 2 * padding, 0, color)
-        dpux = Float((@image.width - 2 * padding)) / (maxx - minx + bar_width)
-        dpuy = Float((@image.height - 2 * padding)) / maxy
-        bars.each do |x,y|
-          rectangle(padding + ((x - minx) * dpux).ceil, @image.height - padding - (y * dpuy).ceil, bar_width, y * dpuy, color)
+        # Values of x and y on borders of plot
+        minx = [bars.collect { |k, _| k }.min, 0].min
+        maxx = [bars.collect { |k, _| k }.max, 0].max
+        miny = [bars.collect { |_, v| v }.min, 0].min
+        maxy = [bars.collect { |_, v| v }.max, 0].max
+        dpux = Float((@image.width - 2 * padding)) / (maxx - minx + bar_width) # Dots per unit for X
+        dpuy = Float((@image.height - 2 * padding)) / (maxy - miny) # Dots per unit for Y
+        rectangle(padding, @image.height - padding - (miny.abs * dpuy).ceil, @image.width - 2 * padding, 1, axis_color) #Axis OX
+        rectangle(padding + (minx.abs * dpux).ceil, padding, 1, @image.height - 2 * padding, axis_color) #Axis OY
+
+        bars.each do |x, y|
+          rectangle(padding + ((x + minx.abs) * dpux).floor,
+                    @image.height - padding - (([y, 0].max + miny.abs) * dpuy).ceil + (y.negative? ? 1 : 0),
+                    bar_width, (y.abs * dpuy).ceil, bars_color)
         end
+
       end
 
       def export(filename)
