238773fa646909b9f7d85dc30228a81655f4700c
Dr0bash <igor2-shamaev@yandex.ru>
2019-10-26 23:43:02 +0300

    Added classes with basic possibilities

--- lib/theory_of_probability.rb
+++ lib/theory_of_probability.rb
@@ -1,35 +1,145 @@
 module Combinatorics
-  def factorial(a)
-    res = 1
-    if a > 0
-      for i in 2..a
-        res *= i
-      end
-    end
+  def factorial(n)
+    res = (1..n).inject(:*) || 1
     res
   end
 
-  def Combination(n, k)
+  def fact(n, k)
+    res = [1,1,1]
+    c = 1
+    if n > 1
+      for i in 2..n
+        c *= i
+        if i == n-k
+          res[1] = c
+        elsif i == k
+          res[2] = c
+        end
+      end
+    end
+    res[0] = c
+    res
+  end
+
+  def combination(n, k)
+    f = fact(n, k)
     if n < k or k <= 0
       -1
     else
-      factorial(n) / (factorial(k) * factorial(n - k))
+      f[0] / (f[2] * f[1])
     end
   end
 
-  def Arrangement(n, k)
+  def arrangement(n, k)
+    f = fact(n, k)
     if n < k or k <= 0
       -1
     else
-      factorial(n) / factorial(n - k)
+      f[0] / f[1]
     end
   end
 
-  def Permutation(n, k)
+  def permutation(n, k)
+    f = fact(n, k)
     if n < k or k <= 0
       -1
     else
-      factorial(n) / factorial(k)
+      f[0] / f[2]
     end
   end
+end
+
+module Cubes
+
+  class Polyhedron
+
+    attr_accessor :csides
+    attr_accessor :sides
+
+    def csides
+      @csides
+    end
+
+    def sides
+      @sides
+    end
+
+    def initialize(sides)
+      @csides = 1
+      @sides = [1]
+      if sides > 1
+        @csides = sides
+        for i in 2..sides
+          @sides << i
+        end
+      end
+    end
+
+    def throw
+      rand(1..@csides)
+    end
+  end
+
+  class Set_Of_Polyhedrons
+    attr_reader :pons
+    attr_reader :percentage
+
+    def initialize(arr)
+      @pons = parse_pons(arr).sort_by{|item| -item.csides}
+      @percentage = count_chance_sum
+    end
+
+    def parse_pons(arr)
+      res = []
+      arr.each do |item|
+        res << Polyhedron.new(item)
+      end
+      return res
+    end
+
+    def count_chance_sum
+      h = Hash.new
+      pons[0].sides.each do |item|
+        h[item.to_s] = 1
+      end
+      n = 0
+      m = 0
+      sum = 0
+      arr3 = @pons[0].sides
+      for i in 1..@pons.size - 1
+        q = Queue.new
+        h1 = Hash.new
+        arr1 = arr3
+        arr3 = Array.new
+        arr2 = @pons[i].sides
+        while m < arr2.size
+          if m == 0
+            q << h[(arr1[n]).to_s]
+            sum += h[(arr1[n]).to_s]
+          end
+          if q.size > arr2.size or m > 0
+            sum -= q.pop
+          end
+          h1[(arr1[n] + arr2[m]).to_s] = sum
+          arr3 << (arr1[n] + arr2[m])
+          if n < arr1.size - 1
+            n += 1
+          else
+            m += 1
+          end
+        end
+        h = h1
+        #puts(h1)
+        h1 = Hash.new
+        n = 0
+        m = 0
+        sum = 0
+      end
+      res = Hash.new
+      fchance = @pons.inject(1) { |mult, item| mult * item.csides }
+      arr3.each {|item| res[item.to_s] = Float(h[item.to_s]) / fchance}
+      res
+    end
+  end
+
 end
