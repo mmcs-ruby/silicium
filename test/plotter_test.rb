95a0a440109faa2e728882ed6454162b6add3492
Mikhail <mikes22@yandex.ru>
2019-10-27 01:31:29 +0300

    Bar chart tests cleanup

--- test/plotter_test.rb
+++ test/plotter_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 require 'plotter'
 require 'chunky_png'
 
@@ -8,12 +8,12 @@ class SiliciumTest < Minitest::Test
   @@dir_ready = File.directory?('tmp') || Dir.mkdir('tmp')
 
   def test_plotter_rectangle
-    #todo write method to remove all *.png from tmp before and after running tests
+    # TODO: write method to remove all *.png from tmp before and after running tests
     filename = 'tmp/rectangle.png'
     File.delete(filename) if File.exist?(filename)
 
     plotter = Image.new(100, 100)
-    #todo encapsulate ChunkyPNG::Color into Plotter class
+    # TODO: encapsulate ChunkyPNG::Color into Plotter class
     plotter.rectangle(20, 30, 50, 60, ChunkyPNG::Color('black @ 0.5'))
     plotter.export(filename)
 
@@ -21,7 +21,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_1st_quadrant
+  def test_bar_chart_1st_quadrant
     filename = 'tmp/bar_chart_1st_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -33,7 +33,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_2nd_quadrant
+  def test_bar_chart_2nd_quadrant
     filename = 'tmp/bar_chart_2nd_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -45,7 +45,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_3rd_quadrant
+  def test_bar_chart_3rd_quadrant
     filename = 'tmp/bar_chart_3rd_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -57,7 +57,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_4th_quadrant
+  def test_bar_chart_4th_quadrant
     filename = 'tmp/bar_chart_4th_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -69,7 +69,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_1st2nd_quadrant
+  def test_bar_chart_1st2nd_quadrant
     filename = 'tmp/bar_chart_1st2nd_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -82,7 +82,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_3rd4th_quadrant
+  def test_bar_chart_3rd4th_quadrant
     filename = 'tmp/bar_chart_3rd4th_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -95,7 +95,7 @@ class SiliciumTest < Minitest::Test
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
 
-  def test_plotter_bar_chart_all_quadrant
+  def test_bar_chart_all_quadrant
     filename = 'tmp/bar_chart_all_quadrant.png'
     File.delete(filename) if File.exist?(filename)
 
@@ -106,5 +106,4 @@ class SiliciumTest < Minitest::Test
     assert(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename.sub('tmp', 'test/resources')))
                .eql?(ChunkyPNG::Image.from_datastream(ChunkyPNG::Datastream.from_file(filename))))
   end
-
 end
