f56322d15a69b086cb22b6d735a21d2af3892da9
Dr0bash <igor2-shamaev@yandex.ru>
2019-10-27 20:13:18 +0300

    reworked polyhedron, added methods

--- test/theory_of_probability_test.rb
+++ test/theory_of_probability_test.rb
@@ -17,4 +17,21 @@ class Test < Minitest::Test
 		assert_equal arrangement(5, 3), 60
 	end
 
+	include Cubes
+
+	def test_chance1
+		s = Set_Of_Polyhedrons.new([6, 6])
+		assert s.percentage["5"] - 0.1111111 < 0.0000001
+	end
+
+	def test_chance2
+		s = Set_Of_Polyhedrons.new([6, 6, 6])
+		assert_equal s.percentage["10"] - 0.125, 0
+	end
+
+	def test_chance3
+		s = Set_Of_Polyhedrons.new([[1,3,5], [2, 4, 6]])
+		assert s.percentage["7"] - 0.3333333 < 0.0000001
+	end
+
 end
