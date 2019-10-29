e8076a37dcdfb5e78ef2c4349db46710f178da4b
dr3amy <arterterone@gmail.com>
2019-10-29 09:27:31 +0300

    added get test

--- test/sparse_test.rb
+++ test/sparse_test.rb
@@ -1,15 +1,29 @@
 require "test_helper"
 
 class SparseTest < Minitest::Test
+  include Silicium::Sparse
   def test_that_it_has_a_version_number
     refute_nil ::Silicium::VERSION
   end
 
+  def test_that_get_works
+    m = SparseMatrix.new(3, 3)
+    m.add(0, 1, 2)
+    m.add(1, 0, 1)
+    m.add(2, 1, 3)
+
+    assert_equal 3, m.get(2, 1)
+  end
+
   def test_that_add_works
-    m = ::Silicium::SparseMatrix.new(3, 3)
+    m = SparseMatrix.new(3, 3)
     m.add(0, 0, 1)
 
     assert_equal [[0, 0, 1]], m.triplets
+
+
   end
 end
+
+
 
