2cae4565827de38a8905c0c4ad90286c259d1ec4
dr3amy <arterterone@gmail.com>
2019-10-29 09:27:05 +0300

    added get method

--- lib/silicium/sparse.rb
+++ lib/silicium/sparse.rb
@@ -1,28 +1,38 @@
 # here goes the Sparse class
 module Silicium
-  class SparseMatrix
-    attr_reader :triplets
+  module Sparse
+    class SparseMatrix
+      attr_reader :triplets
 
-    def initialize(rows, cols)
-      @n = rows
-      @m = cols
-      @triplets = []
-    end
-
-    def add(i, j, x)
-      if i > @n || j > @m || i < 0 || j < 0
-        raise RuntimeError, "out of range"
+      def initialize(rows, cols)
+        @n = rows
+        @m = cols
+        @triplets = []
       end
-      f = false
-      @triplets.each do |item|
-        if item[0] == i && item[1] == j
-          item = [i, j, x]
-          f =  true
-          break
+
+      def add(i, j, x)
+        if i > @n || j > @m || i < 0 || j < 0
+          raise RuntimeError, "out of range"
         end
+        f = false
+        @triplets.each do |item|
+          if item[0] == i && item[1] == j
+            item = [i, j, x]
+            f =  true
+            break
+          end
+        end
+        @triplets.push([i, j, x]) unless f
       end
-      @triplets.push([i, j, x]) unless f
-    end
 
+      def get(i, j)
+        triplets.each do |triplet|
+          if triplet[0] == i && triplet[1] == j
+            return triplet[2]
+          end
+        end
+        return 0
+      end
+    end
   end
 end
