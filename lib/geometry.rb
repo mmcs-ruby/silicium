379ece6f129bd415ee4bdb328e39da3422d04ea4
Oxana <lereenadem@yandex.ru>
2019-10-30 20:25:47 +0300

    Move polygon check and point processing out of convex_hull_2d

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -80,39 +80,43 @@ module Silicium
       oriented_area(a, b, c).positive?
     end
 
+    def not_polygon?(points)
+      points.empty? || points.size == 1 || points.size == 2
+    end
+
+    def put_point_in_part!(part, point, direction)
+      direction = method(direction)
+      while part.size >= 2 && !direction.call(part[part.size - 2], part[part.size - 1], point)
+        part.pop
+      end
+      part.push(point)
+    end
+
     ##
     # Returns an array containing points that are included
     # in the minimal convex hull for a given array of points
     # https://e-maxx.ru/algo/convex_hull_graham
     def minimal_convex_hull_2d(points)
-      return points if points.empty? || points.size == 1 || points.size == 2
+      return points if not_polygon?(points)
 
       points.sort_by! { |p| [p.x, p.y] }
-      p1 = points[0]
-      p2 = points.last
-      up = [p1]
-      down = [p1]
+      first = points[0]
+      last = points.last
+      up = [first]
+      down = [first]
 
       (1...points.size).each do |i|
         point = points[i]
-        if i == points.size - 1 || clockwise(p1, point, p2)
-          while up.size >= 2 && !clockwise(up[up.size - 2], up[up.size - 1], point)
-            up.pop
-          end
-          up.push(point)
+        is_last = i == points.size - 1
+        if is_last || clockwise(first, point, last)
+          put_point_in_part!(up, point, :clockwise)
         end
+        if is_last || counter_clockwise(first, point, last)
+          put_point_in_part!(down, point, :counter_clockwise)
+        end
+      end
 
-        if i == points.size - 1 || counter_clockwise(p1, point, p2)
-          while down.size >= 2 && !counter_clockwise(down[down.size - 2], down[down.size - 1], point)
-            down.pop
-          end
-          down.push(point)
-        end
-      end
-      hull = []
-      (0...up.size).each do |j|
-        hull.push(up[j])
-      end
+      hull = up
       (1..(down.size - 2)).reverse_each do |j|
         hull.push(down[j])
       end
