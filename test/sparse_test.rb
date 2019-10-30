14198758f1e9fbf6e72b635aedc2001dfd827abc
stas1f1 <stas1f1@gmail.com>
2019-10-30 20:21:28 +0300

    added tests for transpose method

--- test/sparse_test.rb
+++ test/sparse_test.rb
@@ -21,9 +21,45 @@ class SparseTest < Minitest::Test
 
     assert_equal [[0, 0, 1]], m.triplets
 
+  end
+
+  def test_matrices_are_cloning
+    m = SparseMatrix.new(3, 3)
+    m.add(0, 0, 1)
+
+    m1 = m.copy
+    m1.add(2, 2, 1)
+
+    assert_equal [[0, 0, 1]], m.triplets
 
   end
+
+  def test_transpose_copy
+    m = SparseMatrix.new(3, 3)
+    m.add(0, 0, 1)
+    m.add(1, 2, 2)
+    m.add(2, 0, 4)
+    m1 = m.transpose
+
+    assert_equal [[0, 0, 1], [1, 2, 2], [2, 0, 4]], m.triplets
+    assert_equal [[0, 0, 1], [2, 1, 2], [0, 2, 4]], m1.triplets
+
+  end
+
+  def test_transpose_original
+    m = SparseMatrix.new(3, 3)
+    m.add(0, 0, 1)
+    m.add(1, 2, 2)
+    m.add(2, 0, 4)
+    m.transpose!
+
+    assert_equal [[0, 0, 1], [2, 1, 2], [0, 2, 4]], m.triplets
+
+  end
+
 end
+
+
 
 
 
