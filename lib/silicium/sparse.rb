190b476f51ab32df25a2b68d861a5d733b921b9d
dr3amy <arterterone@gmail.com>
2019-10-30 23:23:41 +0300

    added some commentaries

--- lib/silicium/sparse.rb
+++ lib/silicium/sparse.rb
@@ -1,7 +1,9 @@
 module Silicium
   require_relative 'trans'
+
   # here goes the sparse module
   module Sparse
+
     # here goes the Sparse class
     class SparseMatrix
       attr_reader :triplets
@@ -12,6 +14,7 @@ module Silicium
         @m = cols
         @triplets = []
       end
+
       # Creates a copy of matrix object
       def copy
         new = SparseMatrix.new(@n, @m)
@@ -20,6 +23,7 @@ module Silicium
         end
         new
       end
+
       # Adds an element to matrix by its position and value
       def add(i, j, x)
         if i > @n || j > @m || i < 0 || j < 0
@@ -35,7 +39,8 @@ module Silicium
         end
         @triplets.push([i, j, x]) unless f
       end
-      # Gets the element by its position 
+
+      # Gets the element by its position
       def get(i, j)
         triplets.each do |triplet|
           if triplet[0] == i && triplet[1] == j
