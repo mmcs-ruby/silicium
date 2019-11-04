480e39589f0e35b5bbf390b1a3585a9899ffa38e
wolwer1ne <wolwerine@yandex.ru>
2019-11-04 18:43:03 +0300

    Refactors polynom_parser

--- lib/polynomial_division.rb
+++ lib/polynomial_division.rb
@@ -1,63 +1,48 @@
-module Silicium
+# frozen_string_literal: true
 
-  class Polynom_division
-  # This function return an array of coefficients that have received by parsing input string in format: "<coeff>*x**<degree>+..."
-  # Even if in your expression don't exist x with some degree, you should to write it with 0 coefficient
-  # Also free term you should to write with 0 degree
-  # Example: "2*x**5-3*x**4+0*x**3+0*x**2-5*x**1-6*x**0"
+module Silicium
+  class PolynomialDivision
+    # This function returns an array of coefficients obtained by parsing input string in format: "<coeff>*x**<degree>+..."
+    # Even if in your expression don't exist x with some degree, you should to write it with 0 coefficient
+    # Also free term you should to write with 0 degree
+    # Example: "2*x**5-3*x**4+0*x**3+0*x**2-5*x**1-6*x**0"
     def polynom_parser(str)
-      sgn_array = []                    # Array of signs
-      if str[0] != '-'
-        sgn_array.push('+')
-      else
-        sgn_array.push('-')
-        str[0] = ''
-      end
+      sgn_array = first_sign(str)
       token = str.split(/[-+]/)
-      (0..str.size-1).each do |i|
-        if str[i] == '-' || str[i] == '+'
-          sgn_array.push(str[i])
-        end
+      (0..str.size - 1).each do |i|
+        sgn_array.push(str[i]) if separator?(str[i])
       end
-      size = token.size - 1
-      coeff = Array.new                 # Array of coefficients
-      (0..size).each do |i|
-        degree = token[i].split('*')    # Split by '*' to get coefficient and degree
-        coeff[i]=degree[0].to_f
-        if sgn_array[i] == '-'
-          coeff[i] *= -1
-        end
+
+      coeff = [] # Array of coefficients
+      token.each_with_index do |token, i|
+        degree = token.split('*') # Split by '*' to get coefficient and degree
+        coeff[i] = degree[0].to_f
+        coeff[i] *= -1 if sgn_array[i] == '-'
       end
       coeff
     end
 
+
     # String implementation of result (optimization part)
     def str_res_impl(coeff_res, sgn_array)
       res_size = coeff_res.size
-      res_exp = ""
-      (0..res_size-1).each do |i|
-        res_exp += ((coeff_res[i].ceil(3)).to_s+"*x**"+(res_size - i - 1).to_s)
-        if sgn_array[i+1] != '-'
-          res_exp += sgn_array[i+1]
-        end
+      res_exp = ''
+      (0..res_size - 1).each do |i|
+        res_exp += (coeff_res[i].ceil(3).to_s + '*x**' + (res_size - i - 1).to_s)
+        res_exp += sgn_array[i + 1] if sgn_array[i + 1] != '-'
       end
       res_exp
     end
 
-    
     # String implementation of remained part (optimization part)
     def str_rem_impl(coeff_1)
       c = coeff_1.size
-      rem_exp = ""
-      (0..c-1).each do |i|
-        if coeff_1[i] >= 0.0
-          rem_exp += '+'
-        end
-        rem_exp += ((coeff_1[i].ceil(3)).to_s+"*x**"+(c - i - 1).to_s)
+      rem_exp = ''
+      (0..c - 1).each do |i|
+        rem_exp += '+' if coeff_1[i] >= 0.0
+        rem_exp += (coeff_1[i].ceil(3).to_s + '*x**' + (c - i - 1).to_s)
       end
-      if rem_exp[0] == '+'
-        rem_exp[0] = ''
-      end
+      rem_exp[0] = '' if rem_exp[0] == '+'
       rem_exp
     end
 
@@ -68,19 +53,35 @@ module Silicium
       coeff_2 = polynom_parser(poly_2)
       res_size = coeff_1.size - coeff_2.size + 1
       coeff_result = Array.new(res_size)
-      sgn_array = Array.new(res_size + 1,'')
-      (0..res_size-1).each do |i|
+      sgn_array = Array.new(res_size + 1, '')
+      (0..res_size - 1).each do |i|
         cur_coeff = coeff_1[i] / coeff_2[0]
         coeff_result[i] = cur_coeff
-        coeff_result[i] < 0 ? sgn_array[i] = '-' : sgn_array[i] = '+'
-        (0..coeff_2.size-1).each do |j|
-          coeff_1[i+j] -= coeff_2[j]*cur_coeff
+        sgn_array[i] = coeff_result[i] < 0 ? '-' : '+'
+        (0..coeff_2.size - 1).each do |j|
+          coeff_1[i + j] -= coeff_2[j] * cur_coeff
         end
       end
       res_exp = str_res_impl(coeff_result, sgn_array)
       rem_exp = str_rem_impl(coeff_1)
       [res_exp, rem_exp]
     end
-  end
-end
 
+    private
+
+    def first_sign(str)
+      if str[0] != '-'
+        result = %w(+)
+      else
+        result = %w(-)
+        str[0] = ''
+      end
+      result
+    end
+
+    def separator?(char)
+      char == '-' || char == '+'
+    end
+  end
+
+end
