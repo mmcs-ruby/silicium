d667c174d5e172ca1f0f2b31a9678c3cdc922dff
gmostovoy <ofglebfortrash@gmail.com>
2020-10-08 17:05:42 +0300

    Start to linear regression

---
+++ lib/linear_regression.rb
@@ -0,0 +1,23 @@
+module Silicium
+  module LinearRegression
+    class LinearRegressionByGradientDescent
+      def generate_function(plot)
+        theta0 = 0
+        theta1 = 0
+        epsilon = 0.1
+        bias = 10
+        unless bias < epsilon
+
+        end
+      end
+
+      def cost_function(plot, theta0, theta1)
+        result = 0
+        m = plot.lenght
+        plot.each do |x, y|
+
+        end
+      end
+    end
+  end
+end
