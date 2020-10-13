6aeaf678d6e2e0ca0a446c1a22dcb6b67a29fd8a
Igor_Win10 <reid09@mail.ru>
2020-10-13 15:04:01 +0300

    refactoring name of def

--- lib/algebra.rb
+++ lib/algebra.rb
@@ -16,13 +16,13 @@ module Silicium
     end
 
     ##
-    # +primes_to(n)+ finds all primes up to n
+    # +primes(n)+ finds all primes up to n
     # with the sieve of eratosthenes
     #
-    ## primes_to(1)		# => []
-    ## primes_to(15)		# => [2, 3, 5, 7, 11, 13]
-    ## primes_to(50)		# => [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-    def primes_to(n)
+    ## primes(1)		# => []
+    ## primes(15)		# => [2, 3, 5, 7, 11, 13]
+    ## primes(50)		# => [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    def primes(n)
       raise ArgumentError unless valid_n?(n)
       arr = (0..n).to_a
       arr[0] = arr[1] = nil
