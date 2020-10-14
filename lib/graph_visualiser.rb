aadba059bd4bb0ca93a3a6f9c9d957dd61176140
barakuda211 <stalkerchnebo@gmail.com>
2020-10-14 20:14:34 +0300

    Creation of files Added graph_visualiser.rb and garph_visualiser_test.rb. Added first test.

--- lib/graph_visualiser.rb
+++ lib/graph_visualiser.rb
@@ -1,5 +1,7 @@
 require 'silicium'
+require 'chunky_png'
 require 'ruby2d'
+require 'ruby2d/window'
 
 
 module Silicium
@@ -7,15 +9,19 @@ module Silicium
   #
   #
   module GraphVisualiser
+    include Silicium::Graphs
+    include Ruby2D
 
     def change_window_size(w, h)
-      set width:w, height:h
+      (Window.get :window).set width: w, height: h
     end
 
     def draw_vertex(x, y, sz, vert)
-      v = Circle.new(x,y,sz)
+      v = Circle.new(x: x, y: y, radius: sz)
     end
 
-
+    def show_window
+      show
+    end
   end
 end
--- test/graph_visualiser_test.rb
+++ test/graph_visualiser_test.rb
@@ -1,1 +1,15 @@
+require 'test_helper'
+require 'graph_visualiser'
+require 'chunky_png'
+require 'ruby2d'
 
+class GraphVisualiserTest < Minitest::Test
+  include Silicium::GraphVisualiser
+
+  def test_window_size
+    change_window_size(500, 300)
+    assert_equal(500, (Window.get :width))
+    assert_equal(300, (Window.get :height))
+  end
+
+end
