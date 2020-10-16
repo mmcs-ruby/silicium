fda310180db8334e33f2eac420138e346caf9754
barakuda211 <stalkerchnebo@gmail.com>
2020-10-16 21:14:58 +0300

    Added initialization for oriented graph

--- lib/graph_visualiser.rb
+++ lib/graph_visualiser.rb
@@ -12,16 +12,59 @@ module Silicium
     include Silicium::Graphs
     include Ruby2D
 
+    public
+
+    ##
+    # Change window and image size
     def change_window_size(w, h)
       (Window.get :window).set width: w, height: h
     end
 
-    def draw_vertex(x, y, sz, vert)
-      v = Circle.new(x: x, y: y, radius: sz)
+    ##
+    # Set graph for visualization
+    def set_graph(graph)
+      if graph.class == OrientedGraph
+        set_oriented_graph(graph)
+      elsif graph.class == UnorientedGraph
+        set_unoriented_graph(graph)
+      elsif
+        raise 'Wrong type of graph!'
+      end
     end
 
+    private
+
+    def set_oriented_graph(graph)
+      @vertices = []
+      w = Window.get :width
+      h = Window.get :height
+      radius= [w,h].max*1.0 / 2 - 50;
+      vert_step = 360.0  / graph.vertex_number
+      position = 0
+      graph.vertices.each do |vert|
+        x = w/2 + Math.cos(position)*radius
+        y = h/2 + Math.sin(position)*radius
+        @vertices << {v: vert ,circ: draw_vertex(x,y,10)}
+        position += vert_step
+      end
+
+    end
+
+    def set_unoriented_graph(graph)
+
+    end
+
+    ##
+    # creates circle for vertex
+    def draw_vertex(x, y, sz)
+      circle = Circle.new(x: x, y: y, radius: sz)
+      return circle
+    end
+
+    ##
+    # show graph on the screen
     def show_window
-      show
+      Window.show
     end
   end
 end
--- test/graph_visualiser_test.rb
+++ test/graph_visualiser_test.rb
@@ -5,11 +5,19 @@ require 'ruby2d'
 
 class GraphVisualiserTest < Minitest::Test
   include Silicium::GraphVisualiser
+  include Silicium::Graphs
 
   def test_window_size
     change_window_size(500, 300)
     assert_equal(500, (Window.get :width))
     assert_equal(300, (Window.get :height))
+  end
+
+  def test_show
+    change_window_size(1000, 600)
+    graph = OrientedGraph.new([{v: 0, i: [:one]}, {v: :one, i: [0, 'two']}, {v: 'two', i: [0, 'two']}])
+    set_graph(graph)
+    show_window
   end
 
 end
