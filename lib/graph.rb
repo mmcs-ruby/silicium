c8e8282f13eb8290baaa1b6adb47e5243dc54329
Denis Shishov <dshishov@sfedu.ru>
2020-10-17 15:11:14 +0300

    add comp graph's gates base

--- lib/graph.rb
+++ lib/graph.rb
@@ -233,19 +233,19 @@ module Silicium
         expr_proc.split.each do |elem|
           case elem
           when "+"
-            dot = Summ_Gate.new(elem)
+            dot = CompGates::Summ_Gate.new(elem)
             dot.connect(pre_graph.pop,pre_graph.pop)
             pre_graph.push(dot)
             @graph.push(dot)
             @size +=1
           when "*"
-            dot = Mult_Gate.new(elem)
+            dot = CompGates::Mult_Gate.new(elem)
             dot.connect(pre_graph.pop,pre_graph.pop)
             pre_graph.push(dot)
             @graph.push(dot)
             @size +=1
           when "/"
-            dot = Div_Gate.new(elem)
+            dot = CompGates::Div_Gate.new(elem)
             scnd = pre_graph.pop
             frst = pre_graph.pop
             dot.connect(frst,scnd)
@@ -253,7 +253,7 @@ module Silicium
             @graph.push(dot)
             @size +=1
           else
-            dot = Comp_Gate.new(elem)
+            dot = CompGates::Comp_Gate.new(elem)
             pre_graph.push(dot)
             @graph.push(dot)
             @size +=1
@@ -263,7 +263,7 @@ module Silicium
       #Compute a value of expression
       def ForwardPass(variables_val)
         @graph.each do |elem|
-          if elem.class != Comp_Gate
+          if elem.class != CompGates::Comp_Gate
             elem.forward_pass
           else
             elem.frwrd = variables_val[elem.name]
@@ -276,7 +276,7 @@ module Silicium
         param_grad = Hash.new()
         @graph.last.bckwrd = loss_value
         @graph.reverse.each do |elem|
-          if elem.class != Comp_Gate
+          if elem.class != CompGates::Comp_Gate
             elem.backward_pass
           else
             param_grad[elem.name] = elem.bckwrd
@@ -366,6 +366,75 @@ module Silicium
     # Implements algorythm of Dijkstra
     def dijkstra_algorythm(graph, starting_vertex)
       #
+    end
+    module CompGates
+      class Comp_Gate
+        attr_accessor :frwrd,:bckwrd,:out,:name
+        def initialize(name)
+          @name = name
+          @frwrd = self
+        end
+      end
+      class Summ_Gate < Comp_Gate
+        attr_accessor :in_frst,:in_scnd
+        def initialize(name)
+          super(name)
+        end
+        def connect(f_n,s_n)
+          @in_frst = f_n
+          @in_scnd = s_n
+          f_n.out = self
+          s_n.out = self
+        end
+        def forward_pass()
+          @frwrd = @in_frst.frwrd + @in_scnd.frwrd
+        end
+        def backward_pass()
+          @in_frst.bckwrd = @bckwrd
+          @in_scnd.bckwrd = @bckwrd
+        end
+
+      end
+      class Mult_Gate < Comp_Gate
+        attr_accessor :in_frst,:in_scnd
+        def initialize(name)
+          super(name)
+        end
+        def connect(f_n,s_n)
+          @in_frst = f_n
+          @in_scnd = s_n
+          f_n.out = self
+          s_n.out = self
+        end
+        def forward_pass()
+          @frwrd = @in_frst.frwrd * @in_scnd.frwrd
+        end
+        def backward_pass()
+          @in_frst.bckwrd = @bckwrd * @in_scnd.frwrd
+          @in_scnd.bckwrd = @bckwrd * @in_frst.frwrd
+        end
+
+      end
+      class Div_Gate < Comp_Gate
+        attr_accessor :in_frst,:in_scnd
+        def initialize(name)
+          super(name)
+        end
+        def connect(f_n,s_n)
+          @in_frst = f_n
+          @in_scnd = s_n
+          f_n.out = self
+          s_n.out = self
+        end
+        def forward_pass()
+          @frwrd = @in_frst.frwrd / @in_scnd.frwrd
+        end
+        def backward_pass()
+          @in_frst.bckwrd = @bckwrd * ((-1)/(@in_scnd.frwrd ** 2))
+          @in_scnd.bckwrd = @bckwrd * ((-1)/(@in_frst.frwrd ** 2))
+        end
+
+      end
     end
   end
 end
