50126f5bcdc06828d9610b49da9c31e8c4f0477f
ms-Anastaisha <ms.urmanana@gmail.com>
2020-10-18 20:54:20 +0300

    cancel robocop changes

--- lib/DFS.rb
+++ lib/DFS.rb
@@ -6,11 +6,13 @@ def depth_first_search?(graph, start, goal)
   until stack.empty?
     node = stack.pop
     raise ArgumentError if graph.vertices[node].nil?
-    return true if node == goal
+    if node == goal
+      return true
+    end
 
-    next if visited[node]
-
-    add_to_stack(graph, node, stack, visited)
+    unless visited[node]
+      add_to_stack(graph, node, stack, visited)
+    end
   end
   false
 end
