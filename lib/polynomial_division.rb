64104b0a7d4209e0783a9b836b1df695d7c700ab
Никита <48802025+eternalforgotten@users.noreply.github.com>
2020-10-26 10:11:45 +0300

    gcd done

--- lib/polynomial_division.rb
+++ lib/polynomial_division.rb
@@ -10,17 +10,18 @@ module Silicium
       # Also free term you should to write with 0 degree
       # Example: "2*x**5-3*x**4+0*x**3+0*x**2-5*x**1-6*x**0"
       def polynom_parser(str)
+        copy_str = str.clone
         sgn_array = []                    # Array of signs
-        if str[0] != '-'
+        if copy_str[0] != '-'
           sgn_array.push('+')
         else
           sgn_array.push('-')
-          str[0] = ''
+          copy_str[0] = ''
         end
-        token = str.split(/[-+]/)
-        (0..str.size-1).each do |i|
-          if str[i] == '-' || str[i] == '+'
-            sgn_array.push(str[i])
+        token = copy_str.split(/[-+]/)
+        (0..copy_str.size-1).each do |i|
+          if copy_str[i] == '-' || copy_str[i] == '+'
+            sgn_array.push(copy_str[i])
           end
         end
         size = token.size - 1
@@ -83,6 +84,51 @@ module Silicium
         res_exp = str_res_impl(coeff_result, sgn_array)
         rem_exp = str_rem_impl(coeff_1[coeff_result.size..coeff_1.size-1])
         [res_exp, rem_exp]
+      end
+
+      # This function returns a string: greatest common integer divisor of two polynoms
+      def polynom_gcd(poly_1, poly_2)
+        coeff_array_1 = polynom_parser(poly_1)
+        coeff_array_2 = polynom_parser(poly_2)
+
+        #"Bigger" polynom should be divided by "smaller" polynom, compare by degree
+        if coeff_array_1.size >= coeff_array_2.size
+          dividend = poly_1
+          divisor = poly_2
+        else
+          dividend = poly_2
+          divisor = poly_1
+        end
+
+        division = polynom_division(dividend, divisor)
+        remainder = division[1]
+
+        #Providing precision because of storage of float numbers
+        until polynom_parser(remainder).all?{|item| item.abs < 0.01} do
+          division = polynom_division(divisor, remainder)
+          new_remainder = division[1]
+          divisor = remainder
+          remainder = new_remainder
+        end
+
+        # Division of greatest common divisor by number does not change the sense of greatest common divisor
+        # So I do this to make the first coefficient of resulting polynom be 1
+        normalizer = polynom_parser(divisor)[0]
+        temp_result = polynom_division(divisor, normalizer.to_s+"*x**0")[0]
+
+        #There still can be an error, so I round coefficients which are really needed in it(condition in map! method)
+        coeffs = polynom_parser(temp_result)
+        coeffs.map! { |element| (element.round - element).abs < 0.01 ? element.round.to_f : element}
+
+        # And then create a new string with valid coefficients - The True Greatest Common Divisor
+        gcd = ""
+        size = coeffs.size
+        for index in 0..(size - 1) do
+          coeff = coeffs[index]
+          plus = (coeff >= 0 and index != 0) ? "+" : ""
+          gcd += "#{plus}#{coeff}*x**#{size - index - 1}"
+        end
+        gcd
       end
     end
   end
