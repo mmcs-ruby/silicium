3b4ebb53ec320e3afd16b7c9adb11f2b9fd126b5
wolwer1ne <wolwerine@yandex.ru>
2020-10-27 10:44:10 +0300

    Refactors tests and dfs

--- lib/DFS.rb
+++ lib/DFS.rb
@@ -5,12 +5,17 @@ def depth_first_search?(graph, start, goal)
   stack = [start]
   until stack.empty?
     node = stack.pop
-    raise ArgumentError if graph.vertices[node].nil?
-    return true if node == goal
+    return true if goal_node?(graph, node, goal)
 
     add_to_stack(graph, node, stack, visited) unless visited[node]
   end
   false
+end
+
+def goal_node?(graph, node, goal)
+  raise ArgumentError if graph.vertices[node].nil?
+
+  node == goal
 end
 
 def add_to_stack(graph, node, stack, visited)
