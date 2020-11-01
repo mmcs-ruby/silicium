562bdc5440f3e5c8699333cacc2642bc7dafb609
asp1208 <asp121208@gmail.com>
2020-11-01 19:45:13 +0300

    Make Polynomial interpolation file

---
+++ lib/polynomial_interpolation.rb
@@ -0,0 +1,25 @@
+
+module Silicium
+
+  module Algebra
+
+    ##
+    # A class providing polynomial interpolation methods
+    class PolynomialInterpolation
+
+      ##
+      # x : array returned by function
+      # y : array of data points
+      # z?????? : the node to interpolate at
+      #
+      def lagrange_polynomials(x,y,t)
+        # TO DO
+          end
+
+
+        end
+
+
+    end
+  end
+
