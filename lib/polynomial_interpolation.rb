521eb1c54d1364738b1dc039c3ac41ae1ccb96d4
asp1208 <asp121208@gmail.com>
2020-11-04 17:29:19 +0300

    add lagrange_polynomials function

--- lib/polynomial_interpolation.rb
+++ lib/polynomial_interpolation.rb
@@ -1,6 +1,4 @@
-
 module Silicium
-
   module Algebra
 
     ##
@@ -8,18 +6,26 @@ module Silicium
     class PolynomialInterpolation
 
       ##
-      # x : array returned by function
-      # y : array of data points
-      # z?????? : the node to interpolate at
-      #
-      def lagrange_polynomials(x,y,t)
-        # TO DO
+      # x : array of data points
+      # y : array returned by function
+      # z : interpolation point
+      def lagrange_polynomials(x, y, z)
+        result = 0.0
+        (0..range(length(y))-1).each do |i|
+          p = 1.0
+          (0..range(length(x))-1).each do |j|
+            if i != j
+              p *= (z - x[j]) / (x[i] - x[j])
+            end
           end
-
-
+          result = result + y[i] * p
         end
+        result
+      end
 
 
+      
     end
   end
+end
 
