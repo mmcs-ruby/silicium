43f0af1d6c31d242baafbd9732e3055d9aa1a6b1
asp1208 <asp121208@gmail.com>
2020-11-06 02:09:19 +0300

    refactor add new tests

--- lib/polynomial_interpolation.rb
+++ lib/polynomial_interpolation.rb
@@ -10,6 +10,7 @@ module Silicium
       # y : array returned by function
       # z : interpolation point
       def self.lagrange_polynomials(x , y , z )
+        check_variables(x, y, z)
         result = 0.0
         y.each_index do |j|
           p1 = 1.0
@@ -25,6 +26,31 @@ module Silicium
         result
       end
 
+      ##
+      # helper for lagrange_polynomials
+      def self.check_variables(x, y, z)
+
+        if x.class != Array || y.class != Array
+          raise ArgumentError, 'Wrong type of variables x or y'
+        end
+
+        if x.empty? ||  y.empty?
+          raise ArgumentError, 'Arrays are empty'
+        end
+
+        if x.size < 2  ||  x.size < 2
+          raise ArgumentError, 'Arrays are too small'
+        end
+
+        if z.class.superclass != Numeric
+          raise ArgumentError, 'Wrong type of variable z'
+        end
+
+        if x[0].class.superclass != Numeric || y[0].class.superclass != Numeric
+          raise ArgumentError, 'Wrong type of arrays'
+        end
+
+      end
 
     end
 
--- test/polynomial_interpolation_test.rb
+++ test/polynomial_interpolation_test.rb
@@ -13,16 +13,34 @@ class PolynomialInterpolationTest < MiniTest::Test
     assert_equal Silicium::Algebra::PolynomialInterpolation::lagrange_polynomials([0, 1, 2, 6], [-1, -3, 3, 1187], 4), 255.0
   end
 
-
-  def setup
-    # Do nothing
+  def test_lagrange_polynomials_with_wrong_variable_z
+    assert_raises ArgumentError do
+      Silicium::Algebra::PolynomialInterpolation::lagrange_polynomials([0, 1, 2, 6], [-7, -1, 1, 43], 'z')
+    end
   end
 
-  def teardown
-    # Do nothing
+  def test_lagrange_polynomials_with_wrong_variable_x
+    assert_raises ArgumentError do
+      Silicium::Algebra::PolynomialInterpolation::lagrange_polynomials("x", [-7, -1, 1, 43], 2 )
+    end
   end
 
-  def test
-    skip 'Not implemented'
+  def test_lagrange_polynomials_with_wrong_type_of_arrays
+    assert_raises ArgumentError do
+      Silicium::Algebra::PolynomialInterpolation::lagrange_polynomials([[1, 2], [2, 3]], [-7, -1, 1, 43], 2 )
+    end
   end
+
+  def test_lagrange_polynomials_with_size_problem
+    assert_raises ArgumentError do
+      Silicium::Algebra::PolynomialInterpolation::lagrange_polynomials([1], [-7, -1, 1, 43], 2 )
+    end
+  end
+
+  def test_lagrange_polynomials_with_empty_arrays
+    assert_raises ArgumentError do
+      Silicium::Algebra::PolynomialInterpolation::lagrange_polynomials([], [-7, -1, 1, 43], 2 )
+    end
+  end
+
 end
