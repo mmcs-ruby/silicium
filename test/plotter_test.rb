400ff2284245305010946b5adb309dc0fa065647
madetara <x@madetara.dev>
2020-11-05 23:39:43 +0300

    refactor(tests): improve cleanup in plotter test
    Store all plots created during test in temp directory. Remove temp
    directory after test run

--- test/plotter_test.rb
+++ test/plotter_test.rb
@@ -1,32 +1,35 @@
 require 'tmpdir'
 require 'test_helper'
 require 'plotter'
+require 'fileutils'
 
 class PlotterTest < Minitest::Test
   include Silicium::Plotter
 
+  TEMP_DIR = "#{Dir.pwd}/tmp"
   PATH_TO_TEST_RESOURCES = 'test/resources'
-  attr_accessor :temp_name
-  attr_accessor :needs_teardown
 
-  def setup
-    @temp_name = Dir::Tmpname.create(%w[plot .png]) {}
-    @needs_teardown = false
-    super
-  end
+  SETUP = begin
+            FileUtils.rm_rf(TEMP_DIR) if Dir.exist?(TEMP_DIR)
+            Dir.mkdir(TEMP_DIR)
+          end
 
-  def teardown
-    File.delete(@temp_name) if @needs_teardown
-    super
+  Minitest.after_run do
+    FileUtils.rm_rf(TEMP_DIR)
   end
 
   def draw_image_and_compare(image_params, file_to_compare)
     plotter = Image.new(image_params[:width], image_params[:height])
     yield plotter
-    @needs_teardown = true
-    plotter.export(@temp_name)
 
-    assert FileUtils.compare_file(@temp_name, File.join(PATH_TO_TEST_RESOURCES, file_to_compare))
+    temp_name = self.get_temp_name
+    plotter.export(temp_name)
+
+    assert FileUtils.compare_file(temp_name, File.join(PATH_TO_TEST_RESOURCES, file_to_compare))
+  end
+
+  def get_temp_name
+    Dir::Tmpname.create(%w[plot .png], TEMP_DIR) {}
   end
 
   def test_plotter_rectangle
@@ -119,4 +122,5 @@ class PlotterTest < Minitest::Test
   def test_plotter_color_2_params
     assert_equal(color('red @ 1.0', nil), 4_278_190_080)
   end
+
 end
