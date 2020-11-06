ab1d70cb282548ce75728d451174cdcd47bf29c8
kadobkina <kadobkina.ea@mail.ru>
2020-11-06 22:39:07 +0300

    mayworks

---
+++ lib/queue.rb
@@ -0,0 +1,204 @@
+# очередь с приоритетами на базе бинарной кучи
+class PQueue
+
+  def initialize(elements=nil, &block)
+  @que = []
+  @cmp = block || lambda{ |a,b| a <=> b }
+  replace(elements) if elements
+  end
+
+  protected
+  attr_reader :que    # базовая куча
+
+  public
+  attr_reader :cmp   # приоритетный компоратор
+
+  # размер очереди
+  def size
+    @que.size
+  end
+
+  # добавление элемента
+  def push(v)
+    @que << v
+    reheap(@que.size-1)
+    self
+  end
+
+  alias :<< :push
+
+  # возвращает наибольший эл-т очереди(голова) и удаляет его, если очередь пуста - возвращает nil
+  def pop
+    return nil if empty?
+    @que.pop
+  end
+
+  # возвращает наименьший эл-т очереди(хвост) и удаляет его, если очередь пуста - возвращает nil
+  def shift
+    return nil if empty?
+    @que.shift
+  end
+
+  # возвращает наибольший эл-т очереди(голова)
+  def top
+    return nil if empty?
+    return @que.last
+  end
+
+  # возвращает наименьший эл-т очереди(хвост)
+  def bottom
+    return nil if empty?
+    return @que.first
+  end
+
+  # добавление сразу нескольких элементов
+  # параметр elems должен удовлетворять to_a(~массив)
+  def concat(elems)
+    if empty?
+      if elems.kind_of?(PQueue)
+        initialize_copy(elems)
+      else
+        replace(elems)
+      end
+    else
+      if elems.kind_of?(PQueue)
+        @que.concat(elems.que)
+        sort!
+      else
+        @que.concat(elems.to_a)
+        sort!
+      end
+    end
+    return self
+  end
+
+  # возвращает первые n элементов очереди с головы, очередь преобразовывается
+  def take(n=@size)
+    a = []
+    n.times{a.push(pop)}
+    a
+  end
+
+  # проверка на пустоту
+  def empty?
+    @que.empty?
+  end
+
+  # удаление всех эл-тов из очереди
+  def clear
+    @que.clear
+    self
+  end
+
+  # преобразование очереди в массив
+  def to_a
+    @que.dup
+  end
+
+  # заменяет эл-ты очереди на новые элементы(elems)
+  # параметр elems должен удовлетворять to_a(~массив)
+  def replace(elems)
+    if elems.kind_of?(PQueue)
+      initialize_copy(elems)
+    else
+      @que.replace(elems.to_a)
+      sort!
+    end
+    self
+  end
+
+  # проверка на наличие эл-та(elem) в очереди
+  def include?(elem)
+    @que.include?(elem)
+  end
+
+  # заменяет наиб эл-т(голову) на elem (приоритетность соблюдается)
+  # возвращает удаленную голову
+  def swap(elem)
+    head = pop
+    push(elem)
+    head
+  end
+
+  # деструктивный перебор всех эл-тов
+  def each_pop
+    until empty?
+      yield if block_given?
+      pop
+    end
+    nil
+  end
+
+  # проверка очередей на равнество
+  def ==(other)
+    size == other.size && to_a == other.to_a
+  end
+
+  private
+  # копия очереди
+  def initialize_copy(other)
+    @cmp  = other.cmp
+    @que  = other.que.dup
+    sort!
+  end
+
+  # ставит elem на "свое" место, относительно приоритета
+  def reheap(elem)
+    return self if size <= 1
+
+    que = @que.dup
+
+    x = que.delete_at(elem)
+    ind = binary_index(que, x)
+
+    que.insert(ind, x)
+
+    @que = que
+
+    return self
+  end
+
+  # сортировка очереди
+  def sort!
+    @que.sort! do |a,b|
+      case @cmp.call(a,b)
+      when  0, nil   then  0
+      when  1, true  then  1
+      when -1, false then -1
+      else
+        warn "bad comparison procedure in #{self.inspect}"
+        0
+      end
+    end
+    self
+  end
+
+  # индекс эл-та x
+  def binary_index(que, x)
+    upper = que.size - 1
+    lower = 0
+
+    while(upper >= lower) do
+      ind  = lower + (upper - lower) / 2
+      comp = @cmp.call(x, que[ind])
+
+      case comp
+      when 0, nil
+        return ind
+      when 1, true
+        lower = ind + 1
+      when -1, false
+        upper = ind - 1
+      else
+      end
+    end
+    lower
+    end
+end # class PQueue
+
+#временный тест
+#q = PQueue.new([2,4,5])
+#q << 6
+#q << 1
+#puts q.to_a
+
---
+++ test/queue_test.rb
@@ -0,0 +1,176 @@
+require 'minitest'
+require 'test_helper'
+require 'queue'
+
+class PQueueTest < Minitest::Test
+
+  ARY_TEST   = [2,6,1,3,8,15,0,-4,7,8,10]
+  ARY_TEST_2 = [25,10,5,13,16,9,16,12]
+
+  def test_initialize_empty
+    PQueue.new
+  end
+
+  def test_initialize_single_element
+    PQueue.new([3])
+  end
+
+  def test_initialize_multiple_elements
+    PQueue.new(ARY_TEST)
+  end
+
+  def test_initialize_with_custom_comparison
+    PQueue.new {|a,b| b<=>a}
+    PQueue.new([3]) {|a,b| b<=>a}
+    PQueue.new(ARY_TEST) {|a,b| b<=>a}
+  end
+
+  def test_top
+    assert_equal(ARY_TEST.max, PQueue.new(ARY_TEST).top)
+    assert_nil(PQueue.new.top)
+  end
+
+  def test_pop
+    sorted_ary = ARY_TEST.sort
+    q = PQueue.new(ARY_TEST)
+    ARY_TEST.size.times do
+      assert_equal(sorted_ary.pop, q.pop)
+    end
+    assert_equal(0, q.size)
+    assert_nil(PQueue.new.pop)
+  end
+
+  def test_insertion
+    q = PQueue.new(ARY_TEST)
+    assert_equal(ARY_TEST.size, q.size)
+
+    ret = q.push(24)
+    assert_equal(q, ret)
+    assert_equal(ARY_TEST.size+1, q.size)
+  end
+
+  def test_concat
+    q = PQueue.new(ARY_TEST)
+
+    ret = q.concat(ARY_TEST_2)
+    assert_equal(q, ret)
+    assert_equal(ARY_TEST.size+ARY_TEST_2.size, q.size)
+
+    q = PQueue.new(ARY_TEST)
+    r = PQueue.new(ARY_TEST_2)
+    q.concat(r)
+    assert_equal(ARY_TEST.size + ARY_TEST_2.size, q.size)
+  end
+
+  def test_clear
+    q = PQueue.new(ARY_TEST).clear
+    assert_equal(q, q.clear)
+    assert_equal(0, q.size)
+  end
+
+  def test_replace
+    q = PQueue.new(ARY_TEST)
+    q.replace(ARY_TEST_2)
+    assert_equal(ARY_TEST_2.size, q.size)
+
+    q = PQueue.new(ARY_TEST)
+    q.replace(PQueue.new(ARY_TEST_2))
+    assert_equal(ARY_TEST_2.size, q.size)
+  end
+
+  def test_inspect
+    assert_equal("<PQueue: size=#{ARY_TEST.size}, top=#{ARY_TEST.max}>",
+                 PQueue.new(ARY_TEST).inspect)
+  end
+
+  def test_to_a
+    q = PQueue.new(ARY_TEST)
+    assert_equal(ARY_TEST.sort, q.to_a)
+    q = PQueue.new(0..4)
+    assert_equal([0,1,2,3,4], q.to_a)
+  end
+
+  def pop_array
+    q = PQueue.new(ARY_TEST)
+    assert_equal(ARY_TEST.sort.reverse[0..5], q.pop_array(5))
+    q = PQueue.new(ARY_TEST)
+    assert_equal(ARY_TEST.sort.reverse, q.pop_array)
+  end
+
+  def test_include
+    q = PQueue.new(ARY_TEST + [21] + ARY_TEST_2)
+    assert_equal(true, q.include?(21))
+
+    q = PQueue.new(ARY_TEST - [15])
+    assert_equal(false, q.include?(15))
+  end
+
+  def test_equal
+    assert_equal(PQueue.new, PQueue.new)
+    assert_equal(PQueue.new(ARY_TEST), PQueue.new(ARY_TEST.sort_by{rand}))
+  end
+
+  def test_swap
+    q = PQueue.new
+    assert_nil(q.swap(6))
+    assert_equal(6, q.top)
+
+    q = PQueue.new(ARY_TEST)
+    h = PQueue.new(ARY_TEST)
+    q.pop; q.push(11)
+    h.swap(11)
+    assert_equal(q, h)
+  end
+
+  def test_dup
+    q = PQueue.new(ARY_TEST)
+    assert_equal(q, q.dup)
+  end
+
+  def test_array_copied
+    ary = ARY_TEST.dup
+    q = PQueue.new(ary)
+    q.pop
+    assert_equal(ARY_TEST, ary)
+
+    ary = ARY_TEST.dup
+    q = PQueue.new
+    q.replace(ary)
+    q.pop
+    assert_equal(ARY_TEST, ary)
+
+    ary = ARY_TEST.dup
+    q = PQueue.new([1])
+    q.concat(ary)
+    q.pop
+    assert_equal(ARY_TEST, ary)
+
+    q = PQueue.new(ARY_TEST)
+    r = q.dup
+    q.pop
+    #    assert_not_equal(q, r)
+  end
+
+  def test_reheap
+    q = PQueue.new([2,4,5])
+    q << 6
+    assert_equal(q.to_a, [2,4,5,6])
+    q << 1
+    assert_equal(q.to_a, [1,2,4,5,6])
+    q << 3
+    assert_equal(q.to_a, [1,2,3,4,5,6])
+
+    q = PQueue.new([100,5,25])
+    assert_equal(q.to_a, [5,25,100])
+    q << 17
+    assert_equal(q.to_a, [5,17,25,100])
+    q << 0
+    assert_equal(q.to_a, [0,5, 7,25,100])
+    q << -5
+    assert_equal(q.to_a, [-5,0,5, 7,25,100])
+    q << 100
+    assert_equal(q.to_a, [-5,0,5, 7,25,100,100])
+  end
+
+
+end
