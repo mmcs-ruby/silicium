577cec10d07c4fb99f5839a472f2101bba65d23a
asp1208 <asp121208@gmail.com>
2020-12-04 20:40:59 +0300

    delete double lagrange

--- lib/polynomial_interpolation.rb
+++ lib/polynomial_interpolation.rb
@@ -25,22 +25,6 @@ module Silicium
         end
         result
       end
-      def self.lagrange_polynomials(x , y , z )
-        check_variables(x, y, z)
-        result = 0.0
-        y.each_index do |j|
-          p1 = 1.0
-          p2 = 1.0
-          x.each_index do |i|
-            if i != j
-              p1 = p1 * (z - x[i])
-              p2 = p2 * (x[j] - x[i])
-            end
-          end
-          result = result + y[j] * p1 / p2
-        end
-        result
-      end
 
 
       # x : array of data points
